945d3c7952a9c2fbe44b984749b2f12bb00dde05
Kevin <k.metallic04@gmail.com>
2018-07-10 19:13:44 +0100

    Part2

--- wamburuKevin.py
+++ wamburuKevin.py
@@ -32,3 +32,66 @@ class Darasa(list):
         for i in range(len(self)):
             print(self[i-1] + "\n")
         return
+
+class Shule:
+    def __init__(self, name):
+        self.shuleName = name
+        self.shule = {}
+        
+    def addDarasa(self, name, darasa):
+        if (len(self.shule) >= 3):
+            print("Failed. Only three classes allowed in a school\n")
+            print("School Name:" + self.shuleName + "\n")
+            for key in self.shule:
+                print(key + ": " + str(self.shule[key]) + "\n")
+            return self
+        else:
+            self.shule[name] = darasa
+            print("School Name:" + self.shuleName + "\n")
+            for key in self.shule:
+                print(key + ": " + str(self.shule[key]) + "\n")
+            return self
+
+    def removeDarasa(self,name):
+        if(self.shule.get(name,1) and len(self.shule) > 0):
+            del self.shule[name]
+            print("School Name:" + self.shuleName + "\n")
+            for key in self.shule:
+                print(key + ": " + str(self.shule[key]) + "\n")
+            return self
+        else:
+            print("Failed. No such class exists\n")
+            return;
+            
+    def getShule(self):
+        print("School Name:" + self.shuleName + "\n")
+        for key in self.shule:
+            print(key + ": " + str(self.shule[key]) + "\n")
+        return self
+    
+
+Silicon_Savannah = Shule("Silicon_Savannah")
+standard1 = Darasa()
+standard1.addClassMember("Donald Trump")
+standard1.addClassMember("Marylin Monroe")
+standard1.addClassMember("Ayn Rand")
+standard1.addClassMember("Jeff Bezos")
+standard1.removeClassMember("Jeff Bezos")
+standard2 = Darasa()
+standard2.addClassMember("Chris Bosh")
+standard2.addClassMember("LeBron James")
+standard2.addClassMember("Kevin Durant")
+standard1.addClassMember("Stephen Curry")
+Silicon_Savannah.addDarasa("standard1", standard1)
+Silicon_Savannah.addDarasa("standard2", standard2)
+Silicon_Savannah.removeDarasa("standard1")
+Silicon_Savannah.getShule()
+Silicon_Savannah.addDarasa("standard1", standard1)
+standard3 = Darasa()
+standard3.addClassMember("Lady Gaga")
+standard3.addClassMember("Chris Brown")
+standard3.addClassMember("TY Dollar")
+Silicon_Savannah.addDarasa("standard3", standard3)
+Silicon_Savannah.getShule()
+
+

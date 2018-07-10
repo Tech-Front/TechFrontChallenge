49d7c6d8f48c96f317537f804abb4aeeb2fb112b
Scopeman <jsang275@gmail.com>
2018-07-11 01:55:02 +0300

    Part 2 completed

--- src/tfc/SangJoseph.java
+++ src/tfc/SangJoseph.java
@@ -1,21 +1,48 @@
 package tfc;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 public class SangJoseph {
     public static void main(String[] args) {
-        // create instance
-        Darasa darasa = new Darasa();
+        // create instances
+        Darasa standard1 = new Darasa();
+        Darasa standard2 = new Darasa();
+        Darasa standard3 = new Darasa();
 
         // add members
-        darasa.addClassMember("Joseph Sang");
-        darasa.addClassMember("James Jones");
-        darasa.addClassMember("Liz Wanjiru");
+        standard1.addClassMember("Githeko");
+        standard1.addClassMember("Muschiri");
 
-        // remove member
-        darasa.removeClassMember("James Jones");
+        standard2.addClassMember("Wambugu");
+        standard2.addClassMember("Kamande");
 
-        // retrieve print out class members
-        for (String name : darasa.getDarasaMembers()) {
-            System.out.println(name);
+        standard3.addClassMember("Joseph");
+        standard3.addClassMember("Kibet");
+
+        // create school
+        Shule hillSchool = new Shule("Hill School");
+        // add darasas
+        hillSchool.addDarasa("Standard 1", standard1);
+        hillSchool.addDarasa("Standard 2", standard2);
+        hillSchool.addDarasa("Standard 3", standard3);
+
+        // display all classes and students
+        List list = hillSchool.getShule();
+        System.out.println("School: " + list.get(0));
+        HashMap dictionary = (HashMap<String, Darasa>) list.get(1);
+        System.out.println("--");
+        Iterator iterator = dictionary.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry pair = (Map.Entry) iterator.next();
+            System.out.println("Class: " + pair.getKey());
+            Darasa darasa = (Darasa) pair.getValue();
+            for (String name : darasa.getDarasaMembers()) {
+                System.out.println(name);
+            }
+            System.out.println("--");
         }
     }
 }

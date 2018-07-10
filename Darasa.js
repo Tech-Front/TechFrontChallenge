4c94c33009731133ce7910c8c8ee28af9e03307c
Victor Kariuki <karizvic@gmail.com>
2018-07-10 23:46:12 +0300

    inital commit

---
+++ Darasa.js
@@ -0,0 +1,29 @@
+function Darasa(classMember) {
+  this.classMembers = classMembers;
+
+  // Add a class member
+  this.addClassMember = function(memberName) {
+    if (this.members.indexOf(memberName) < 0) {
+      this.classMembers.push(classMembers);
+      return 'A members has been added';
+    } else {
+      return 'The class member already exists';
+    }
+  }
+
+  // Remove a class member
+  this.removeClassMember = function(memberName) {
+    for (var i = 0; i < classMembers.length; i++) {
+      if (this.classMembers[i] == memberName) {
+        this.classMembers.splice(i, 1);
+        return 'A member has been removed';
+      }
+    }
+  }
+
+  // Display the members
+  this.displayClassMember = function() {
+    return this.classMembers;
+  }
+}
+}

89e258f6700bc6e6891d1d73de25445a763b5b52
Shiezy <sheilagithinji@gmail.com>
2018-07-10 12:21:26 +0300

    Final copy of my class

--- Darasa.java
+++ Darasa.java
@@ -11,25 +11,17 @@ public class Darasa {
 
   private static List<String> students = new ArrayList<>();
   
-  public static String addMember(String name){
-//      int i;
-//      for(i = 0; i<students.size(); i++){
-//          if(students.get(i) == name){
-//              return "member exists";
-//          }
-//      }
-//      students.add(name);   
-//       return name;
- if(students.contains(name)){
-       return "Member already Exist";
-      }else{
-        students.add(name);
-        return name;
-      }
+  public static String addClassMember(String name){
+    if(students.contains(name)){
+        return "Member already Exist";
+        }else{
+            students.add(name);
+            return name;
+        }
       
   }
   
-  public static String removeMember(String name){
+  public static String removeClassMember(String name){
       if(students.contains(name)){
           students.remove(name);
           return name;
@@ -40,11 +32,15 @@ public class Darasa {
       
   }
 
+  public static List getClassMembers(){
+      return students;
+  }
+
     public static void main(String[] args) {
            Scanner sc=new Scanner(System.in);  
            System.out.println("Enter A students name");  
            String name = sc.next();
-           String newMember = Darasa.addMember(name);
+           String newMember = Darasa.addClassMember(name);
            
           if(newMember.equals(name)){
               System.out.println("Our new member is "+newMember);
@@ -54,15 +50,14 @@ public class Darasa {
            
            System.out.println("Enter the name of the student you wish to remove"); 
            String leavingStudentName = sc.next();
-           String leavingStudent = Darasa.removeMember(leavingStudentName);
+           String leavingStudent = Darasa.removeClassMember(leavingStudentName);
             if(leavingStudent.equals(leavingStudentName)){
                 System.out.println("Our new member is "+leavingStudent);
             }else{
                System.out.println(leavingStudent);
           }
-           
-
-         
+            System.out.println("Class Members Are");
+            System.out.println(Darasa.getClassMembers());
 
 
     }

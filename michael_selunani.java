c23ff46a7e9f1f66d895c9c225a32ddb821b11a5
Michael Se-luani <35915767+Mse-lunani@users.noreply.github.com>
2018-07-10 22:19:33 +0300

    Create michael_selunani.java

---
+++ michael_selunani.java
@@ -0,0 +1,67 @@
+
+package techfrontchallenge;
+
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Michael Se-lunani
+ */
+
+
+public class TechFrontChallenge {
+    // declare array to add members
+static String member[]= new String[100]; 
+// declare index position for the added members
+static int position=0;
+    /**
+     * @param name
+     * @param args the command line arguments
+     * @return 
+     */
+public String add_member(String name){
+    
+  member[position]= name;
+   return name;
+}
+public String remove_member(String element){
+    int length=0;
+   	for (int i = 0; i < member.length; i++) {
+			if(member[i]!=element)
+			{
+				member[length++]=member[i];
+			}
+		}
+    
+    return element;
+}
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        System.out.println("how many members are you adding");
+        
+        Scanner  input = new Scanner(System.in);
+       int all = input.nextInt();
+        for(position=0;position<all;position++){
+            System.out.println("add new member");
+            String new_member = input.next();
+            
+           String pp= new TechFrontChallenge().add_member(new_member);
+            
+            
+            
+        }
+        //delete duplicate value
+      for(position=0;position<all;position++){
+          new TechFrontChallenge().remove_member(member[position]);
+          System.out.println(position+":"+member[position]);
+      }
+        
+        
+       
+        
+        }
+    
+}

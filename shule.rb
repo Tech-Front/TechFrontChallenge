995d01f8492271c243db4fcc9500582e2ba3de92
Daniel Otieno <danielotieno.ke@gmail.com>
2018-07-12 13:02:26 +0300

    Add shule methods

--- shule.rb
+++ shule.rb
@@ -6,6 +6,22 @@ class Shule
   attr_accessor :darasa
   def initialize name
     @name = name
-    @darasa = Darasa.new([])
+    @darasas = Hash.new
+  end
+
+  def add_darasa name, darasa
+        darasas[name] = darasa
+  end
+
+  def remove_darasa name
+    darasas.delete(name)
+  end
+
+  def get_shule
+    puts darasas
   end
 end
+
+standard1 = Darasa.new(['john', 'jane', 'jimmy'])
+standard2 = Darasa.new(['daniel', 'duncan', 'danny'])
+standard3 = Darasa.new(['peter', 'ann', 'mike'])

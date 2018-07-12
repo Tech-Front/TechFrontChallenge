73f94c26b08927b128d5ee18105c44d07fb92aa0
Joel Machango <joelmachango@gmail.com>
2018-07-12 17:17:32 +0300

    Add new member into Darasa
    -If member exists, alert user

--- machangoJoel.py
+++ machangoJoel.py
@@ -1,3 +1,11 @@
 class Darasa:
     def __init__(self):
         self.members = []
+    
+    """
+    adds a new  member into the Darasa.  
+    """
+    def addClassMember(self, member):        
+        if member in self.members:
+            return 'Member exists'
+        self.members.append(member)

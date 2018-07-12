41f7731684c86d1fff565625be0b12e6a0e3c1d1
Daniel Otieno <danielotieno.ke@gmail.com>
2018-07-12 09:26:03 +0300

    Create AddClassMethod and add member

--- danielOtieno.rb
+++ danielOtieno.rb
@@ -3,3 +3,11 @@ class Darasa
     def initialize *members
         @members = members.uniq.collect { |member| member.to_s}
     end
+
+    def addClassMember
+        unless find member
+            self.members << member
+        else
+            puts "Sorry #{member} already exists"
+        end
+    end
